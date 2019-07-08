91086bfb9a92cc36e60812c94b2bea178ad6622f
YusukeKyokawa <kiyo@YusukeKyokawanoMacBook-Pro.local>
2019-07-08 23:53:13 +0900

    functionsを追加

--- Project/kiyo/layers.py
+++ Project/kiyo/layers.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import numpy as np
-from common.functions import *
-from common.util import im2col, col2im
+from functions import *
+from util import im2col, col2im
 
 
 class Relu:
